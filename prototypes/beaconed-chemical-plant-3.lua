5199879a4fc639452cd48efe175befc92085b8a0
Vaclav Benc <vaclav.benc@factorio.com>
2019-07-14 16:35:26 +0200

    Rewrote beaconed chemical plants.

--- prototypes/beaconed-chemical-plant-3.lua
+++ prototypes/beaconed-chemical-plant-3.lua
@@ -1,3 +1,37 @@
+beaconed_chemical_plant_3 = util.table.deepcopy(data.raw["assembling-machine"]["chemical-plant"])
+beaconed_chemical_plant_3.name = "beaconed-chemical-plant-3"
+beaconed_chemical_plant_3.icon = "__Absorbed-Beacons__/graphics/icons/beaconed-chemical-plant-3.png"
+beaconed_chemical_plant_3.minable.result = "beaconed-chemical-plant-3"
+beaconed_chemical_plant_3.crafting_speed = 6.55
+beaconed_chemical_plant_3.energy_source.emissions_per_second_per_watt = 4 / 210000
+beaconed_chemical_plant_3.energy_source.drain = "3367kW"
+beaconed_chemical_plant_3.energy_usage = "2478kW"
+beaconed_chemical_plant_3.allowed_effects = {"productivity", "pollution"}
+beaconed_chemical_plant_3.fast_replaceable_group = "chemical-plant"
+
+beaconed_chemical_plant_3.module_specification.module_slots = data.raw["assembling-machine"]["chemical-plant"].module_specification.module_slots * 2
+if settings.startup["show-module-slot-row-length"].value > 0 then
+  beaconed_chemical_plant_3.module_specification.module_info_max_icons_per_row = settings.startup["show-module-slot-row-length"].value
+end
+if settings.startup["show-module-slot-rows"].value > 0 then
+  beaconed_chemical_plant_3.module_specification.module_info_max_icon_rows = settings.startup["show-module-slot-rows"].value
+end
+
+data:extend({
+  beaconed_chemical_plant_3
+})
+data:extend({
+  {
+    type = "item",
+    name = "beaconed-chemical-plant-3",
+    icon = "__Absorbed-Beacons__/graphics/icons/beaconed-chemical-plant-3.png",
+    icon_size = 32,
+    subgroup = "absorbed-beacons-3",
+    order = "e[chemical-plant]",
+    place_result = "beaconed-chemical-plant-3",
+    stack_size = 10
+  },
+})
 data:extend({
   {
     type = "recipe",
@@ -19,246 +53,4 @@ data:extend({
     allow_as_intermediate = false,
     main_product = "beaconed-chemical-plant-3"
   }
-})
-data:extend({
-  {
-    type = "item",
-    name = "beaconed-chemical-plant-3",
-    icon = "__Absorbed-Beacons__/graphics/icons/beaconed-chemical-plant-3.png",
-    icon_size = 32,
-    subgroup = "absorbed-beacons-3",
-    order = "e[chemical-plant]",
-    place_result = "beaconed-chemical-plant-3",
-    stack_size = 10
-  },
-})
-data:extend({
-  {
-    type = "assembling-machine",
-    name = "beaconed-chemical-plant-3",
-    icon = "__Absorbed-Beacons__/graphics/icons/beaconed-chemical-plant-3.png",
-    icon_size = 32,
-    flags = {"placeable-neutral","placeable-player", "player-creation"},
-    minable = {mining_time = 0.1, result = "beaconed-chemical-plant-3"},
-    max_health = 300,
-    corpse = "big-remnants",
-    dying_explosion = "medium-explosion",
-    collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
-    selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
-    drawing_box = {{-1.5, -1.9}, {1.5, 1.5}},
-    module_specification =
-    {
-      module_slots = 6,
-      module_info_max_icons_per_row = 1,
-      module_info_max_icon_rows = 1,
-    },
-    allowed_effects = {"productivity", "pollution"},
-    fast_replaceable_group = "chemical-plant",
-    animation = make_4way_animation_from_spritesheet({ layers =
-    {
-      {
-        filename = "__base__/graphics/entity/chemical-plant/chemical-plant.png",
-        width = 122,
-        height = 134,
-        frame_count = 1,
-        shift = util.by_pixel(-5, -4.5),
-        hr_version =
-        {
-          filename = "__base__/graphics/entity/chemical-plant/hr-chemical-plant.png",
-          width = 244,
-          height = 268,
-          frame_count = 1,
-          shift = util.by_pixel(-5, -4.5),
-          scale = 0.5
-          }
-      },
-      {
-        filename = "__base__/graphics/entity/chemical-plant/chemical-plant-shadow.png",
-        width = 175,
-        height = 141,
-        frame_count = 1,
-        shift = util.by_pixel(31.5, 11),
-        draw_as_shadow = true,
-        hr_version =
-        {
-          filename = "__base__/graphics/entity/chemical-plant/hr-chemical-plant-shadow.png",
-          width = 350,
-          height = 219,
-          frame_count = 1,
-          shift = util.by_pixel(31.5, 10.75),
-          draw_as_shadow = true,
-          scale = 0.5
-          }
-      }
-    }}),
-    working_visualisations =
-    {
-      {
-        north_position = util.by_pixel(30, -24),
-        west_position = util.by_pixel(1, -49.5),
-        south_position = util.by_pixel(-30, -48),
-        east_position = util.by_pixel(-11, -1),
-        apply_recipe_tint = "primary",
-        animation =
-        {
-          filename = "__base__/graphics/entity/chemical-plant/boiling-green-patch.png",
-          frame_count = 32,
-          width = 15,
-          height = 10,
-          animation_speed = beaconed_chemical_plant_3_animation_speed,
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/chemical-plant/hr-boiling-green-patch.png",
-            frame_count = 32,
-            width = 30,
-            height = 20,
-            animation_speed = beaconed_chemical_plant_3_animation_speed,
-            scale = 0.5
-          }
-        }
-      },
-
-      {
-        north_position = util.by_pixel(30, -24),
-        west_position = util.by_pixel(1, -49.5),
-        south_position = util.by_pixel(-30, -48),
-        east_position = util.by_pixel(-11, -1),
-        apply_recipe_tint = "secondary",
-        animation =
-        {
-          filename = "__base__/graphics/entity/chemical-plant/boiling-green-patch-mask.png",
-          frame_count = 32,
-          width = 15,
-          height = 10,
-          animation_speed = 0.5,
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/chemical-plant/hr-boiling-green-patch-mask.png",
-            frame_count = 32,
-            width = 30,
-            height = 20,
-            animation_speed = 0.5,
-            scale = 0.5
-          }
-        }
-      },
-
-
-      {
-        apply_recipe_tint = "tertiary",
-        north_position = {0, 0},
-        west_position = {0, 0},
-        south_position = {0, 0},
-        east_position = {0, 0},
-        north_animation =
-        {
-          filename = "__base__/graphics/entity/chemical-plant/boiling-window-green-patch.png",
-          frame_count = 1,
-          width = 87,
-          height = 60,
-          shift = util.by_pixel(0, -5),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/chemical-plant/hr-boiling-window-green-patch.png",
-            x = 0,
-            frame_count = 1,
-            width = 174,
-            height = 119,
-            shift = util.by_pixel(0, -5.25),
-            scale = 0.5
-          }
-        },
-        east_animation =
-        {
-          filename = "__base__/graphics/entity/chemical-plant/boiling-window-green-patch.png",
-          x = 87,
-          frame_count = 1,
-          width = 87,
-          height = 60,
-          shift = util.by_pixel(0, -5),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/chemical-plant/hr-boiling-window-green-patch.png",
-            x = 174,
-            frame_count = 1,
-            width = 174,
-            height = 119,
-            shift = util.by_pixel(0, -5.25),
-            scale = 0.5
-          }
-        },
-        south_animation =
-        {
-          filename = "__base__/graphics/entity/chemical-plant/boiling-window-green-patch.png",
-          x = 174,
-          frame_count = 1,
-          width = 87,
-          height = 60,
-          shift = util.by_pixel(0, -5),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/chemical-plant/hr-boiling-window-green-patch.png",
-            x = 348,
-            frame_count = 1,
-            width = 174,
-            height = 119,
-            shift = util.by_pixel(0, -5.25),
-            scale = 0.5
-          }
-        }
-      }
-    },
-    vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
-    working_sound =
-    {
-      sound =
-      {
-        {
-          filename = "__base__/sound/chemical-plant.ogg",
-          volume = 0.8
-        }
-      },
-      idle_sound = { filename = "__base__/sound/idle1.ogg", volume = 0.6 },
-      apparent_volume = 1.5
-    },
-    crafting_speed = 6.55,
-    energy_source =
-    {
-      type = "electric",
-      usage_priority = "secondary-input",
-      emissions_per_second_per_watt = 4 / 210000,
-      drain = "3367kW"
-    },
-    energy_usage = "2478kW",
-    crafting_categories = {"chemistry"},
-    fluid_boxes =
-    {
-      {
-        production_type = "input",
-        pipe_covers = pipecoverspictures(),
-        base_area = 10,
-        base_level = -1,
-        pipe_connections = {{ type="input", position = {-1, -2} }}
-      },
-      {
-        production_type = "input",
-        pipe_covers = pipecoverspictures(),
-        base_area = 10,
-        base_level = -1,
-        pipe_connections = {{ type="input", position = {1, -2} }}
-      },
-      {
-        production_type = "output",
-        pipe_covers = pipecoverspictures(),
-        base_level = 1,
-        pipe_connections = {{ position = {-1, 2} }}
-      },
-      {
-        production_type = "output",
-        pipe_covers = pipecoverspictures(),
-        base_level = 1,
-        pipe_connections = {{ position = {1, 2} }}
-      }
-    }
-  },
 })
