0ad5f54bf2d2ca122bb7823dc1ba2ef8ba4f9f1c
Vaclav Benc <vaclav.benc@factorio.com>
2019-07-14 18:35:31 +0200

    Rewrote beaconed labs.

--- prototypes/beaconed-lab-2.lua
+++ prototypes/beaconed-lab-2.lua
@@ -1,3 +1,113 @@
+beaconed_lab_2 = util.table.deepcopy(data.raw["lab"]["lab"])
+beaconed_lab_2.name = "beaconed-lab-2"
+beaconed_lab_2.icon = "__Absorbed-Beacons__/graphics/icons/beaconed-lab-2.png"
+beaconed_lab_2.minable.result = "beaconed-lab-2"
+beaconed_lab_2.next_upgrade = "beaconed-lab-3"
+beaconed_lab_2.researching_speed = 4.3
+beaconed_lab_2.energy_source.drain = "3360kW"
+beaconed_lab_2.energy_usage = "564kW"
+beaconed_lab_2.allowed_effects = {"productivity", "pollution"}
+beaconed_lab_2.fast_replaceable_group = "lab"
+
+beaconed_lab_2.module_specification.module_slots = data.raw["lab"]["lab"].module_specification.module_slots * 2
+
+if settings.startup["show-module-slot-row-length"].value > 0 then
+  beaconed_lab_2.module_specification.module_info_max_icons_per_row = settings.startup["show-module-slot-row-length"].value
+end
+if settings.startup["show-module-slot-rows"].value > 0 then
+  beaconed_lab_2.module_specification.module_info_max_icon_rows = settings.startup["show-module-slot-rows"].value
+end
+
+if settings.startup["modded-entity-graphics"].value == true then
+  beaconed_lab_2.on_animation = 
+  {
+    layers =
+    {
+      {
+        filename = "__Absorbed-Beacons__/graphics/entity/beaconed-lab/lab-yellow.png",
+        width = 98,
+        height = 87,
+        frame_count = 33,
+        line_length = 11,
+        animation_speed = 1 / 3,
+        shift = util.by_pixel(0, 1.5),
+        hr_version =
+        {
+          filename = "__Absorbed-Beacons__/graphics/entity/beaconed-lab/hr-lab-yellow.png",
+          width = 194,
+          height = 174,
+          frame_count = 33,
+          line_length = 11,
+          animation_speed = 1 / 3,
+          shift = util.by_pixel(0, 1.5),
+          scale = 0.5
+        }
+      },
+      {
+        filename = "__base__/graphics/entity/lab/lab-integration.png",
+        width = 122,
+        height = 81,
+        frame_count = 1,
+        line_length = 1,
+        repeat_count = 33,
+        animation_speed = 1 / 3,
+        shift = util.by_pixel(0, 15.5),
+        hr_version =
+        {
+          filename = "__base__/graphics/entity/lab/hr-lab-integration.png",
+          width = 242,
+          height = 162,
+          frame_count = 1,
+          line_length = 1,
+          repeat_count = 33,
+          animation_speed = 1 / 3,
+          shift = util.by_pixel(0, 15.5),
+          scale = 0.5
+        }
+      },
+      {
+        filename = "__base__/graphics/entity/lab/lab-shadow.png",
+        width = 122,
+        height = 68,
+        frame_count = 1,
+        line_length = 1,
+        repeat_count = 33,
+        animation_speed = 1 / 3,
+        shift = util.by_pixel(13, 11),
+        draw_as_shadow = true,
+        hr_version =
+        {
+          filename = "__base__/graphics/entity/lab/hr-lab-shadow.png",
+          width = 242,
+          height = 136,
+          frame_count = 1,
+          line_length = 1,
+          repeat_count = 33,
+          animation_speed = 1 / 3,
+          shift = util.by_pixel(13, 11),
+          scale = 0.5,
+          draw_as_shadow = true
+        }
+      }
+    }
+  }
+end
+
+data:extend({
+  beaconed_lab_2
+})
+data:extend({
+  {
+    type = "item",
+    name = "beaconed-lab-2",
+    icon = "__Absorbed-Beacons__/graphics/icons/beaconed-lab-2.png",
+    icon_size = 32,
+    subgroup = "absorbed-beacons-2",
+    order = "g[lab]",
+    place_result = "beaconed-lab-2",
+    stack_size = 10
+  },
+})
 data:extend({
   {
     type = "recipe",
@@ -19,198 +129,4 @@ data:extend({
     allow_as_intermediate = false,
     main_product = "beaconed-lab-2"
   }
-})
-data:extend({
-  {
-    type = "item",
-    name = "beaconed-lab-2",
-    icon = "__Absorbed-Beacons__/graphics/icons/beaconed-lab-2.png",
-    icon_size = 32,
-    subgroup = "absorbed-beacons-2",
-    order = "g[lab]",
-    place_result = "beaconed-lab-2",
-    stack_size = 10
-  },
-})
-data:extend({
-  {
-    type = "lab",
-    name = "beaconed-lab-2",
-    icon = "__Absorbed-Beacons__/graphics/icons/beaconed-lab-2.png",
-    icon_size = 32,
-    flags = {"placeable-player", "player-creation"},
-    minable = {mining_time = 0.2, result = "beaconed-lab-2"},
-    max_health = 150,
-    corpse = "big-remnants",
-    dying_explosion = "medium-explosion",
-    collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
-    selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
-    light = {intensity = 0.75, size = 8, color = {r = 1.0, g = 1.0, b = 1.0}},
-    fast_replaceable_group = "lab",
-    next_upgrade = "beaconed-lab-3",
-    on_animation =
-    {
-      layers =
-      {
-        {
-          filename = "__Absorbed-Beacons__/graphics/entity/beaconed-lab/lab-yellow.png",
-          width = 98,
-          height = 87,
-          frame_count = 33,
-          line_length = 11,
-          animation_speed = 1 / 3,
-          shift = util.by_pixel(0, 1.5),
-          hr_version =
-          {
-            filename = "__Absorbed-Beacons__/graphics/entity/beaconed-lab/hr-lab-yellow.png",
-            width = 194,
-            height = 174,
-            frame_count = 33,
-            line_length = 11,
-            animation_speed = 1 / 3,
-            shift = util.by_pixel(0, 1.5),
-            scale = 0.5
-          }
-        },
-        {
-          filename = "__base__/graphics/entity/lab/lab-integration.png",
-          width = 122,
-          height = 81,
-          frame_count = 1,
-          line_length = 1,
-          repeat_count = 33,
-          animation_speed = 1 / 3,
-          shift = util.by_pixel(0, 15.5),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/lab/hr-lab-integration.png",
-            width = 242,
-            height = 162,
-            frame_count = 1,
-            line_length = 1,
-            repeat_count = 33,
-            animation_speed = 1 / 3,
-            shift = util.by_pixel(0, 15.5),
-            scale = 0.5
-          }
-        },
-        {
-          filename = "__base__/graphics/entity/lab/lab-shadow.png",
-          width = 122,
-          height = 68,
-          frame_count = 1,
-          line_length = 1,
-          repeat_count = 33,
-          animation_speed = 1 / 3,
-          shift = util.by_pixel(13, 11),
-          draw_as_shadow = true,
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/lab/hr-lab-shadow.png",
-            width = 242,
-            height = 136,
-            frame_count = 1,
-            line_length = 1,
-            repeat_count = 33,
-            animation_speed = 1 / 3,
-            shift = util.by_pixel(13, 11),
-            scale = 0.5,
-            draw_as_shadow = true
-          }
-        }
-      }
-    },
-    off_animation =
-    {
-      layers =
-      {
-        {
-          filename = "__base__/graphics/entity/lab/lab.png",
-          width = 98,
-          height = 87,
-          frame_count = 1,
-          shift = util.by_pixel(0, 1.5),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/lab/hr-lab.png",
-            width = 194,
-            height = 174,
-            frame_count = 1,
-            shift = util.by_pixel(0, 1.5),
-            scale = 0.5
-          }
-        },
-        {
-          filename = "__base__/graphics/entity/lab/lab-integration.png",
-          width = 122,
-          height = 81,
-          frame_count = 1,
-          shift = util.by_pixel(0, 15.5),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/lab/hr-lab-integration.png",
-            width = 242,
-            height = 162,
-            frame_count = 1,
-            shift = util.by_pixel(0, 15.5),
-            scale = 0.5
-          }
-        },
-        {
-          filename = "__base__/graphics/entity/lab/lab-shadow.png",
-          width = 122,
-          height = 68,
-          frame_count = 1,
-          shift = util.by_pixel(13, 11),
-          draw_as_shadow = true,
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/lab/hr-lab-shadow.png",
-            width = 242,
-            height = 136,
-            frame_count = 1,
-            shift = util.by_pixel(13, 11),
-            draw_as_shadow = true,
-            scale = 0.5
-          }
-        }
-      }
-    },
-    working_sound =
-    {
-      sound =
-      {
-        filename = "__base__/sound/lab.ogg",
-        volume = 0.7
-      },
-      apparent_volume = 1
-    },
-    vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
-    energy_source =
-    {
-      type = "electric",
-      usage_priority = "secondary-input",
-      drain = "3360kW",
-    },
-    energy_usage = "564kW",
-    researching_speed = 4.3,
-    inputs =
-    {
-      "automation-science-pack",
-      "logistic-science-pack",
-      "chemical-science-pack",
-      "military-science-pack",
-      "production-science-pack",
-      "utility-science-pack",
-      "space-science-pack"
-    },
-    allowed_effects = {"productivity", "pollution"},
-    module_specification =
-    {
-      module_slots = 4,
-      module_info_max_icons_per_row = 1,
-      module_info_max_icon_rows = 1,
-      module_info_icon_shift = {0, 0.9}
-    }
-  },
 })
--- prototypes/beaconed-lab-3.lua
+++ prototypes/beaconed-lab-3.lua
@@ -1,3 +1,112 @@
+beaconed_lab_3 = util.table.deepcopy(data.raw["lab"]["lab"])
+beaconed_lab_3.name = "beaconed-lab-3"
+beaconed_lab_3.icon = "__Absorbed-Beacons__/graphics/icons/beaconed-lab-3.png"
+beaconed_lab_3.minable.result = "beaconed-lab-3"
+beaconed_lab_3.researching_speed = 6.7
+beaconed_lab_3.energy_source.drain = "3360kW"
+beaconed_lab_3.energy_usage = "660kW"
+beaconed_lab_3.allowed_effects = {"productivity", "pollution"}
+beaconed_lab_3.fast_replaceable_group = "lab"
+
+beaconed_lab_3.module_specification.module_slots = data.raw["lab"]["lab"].module_specification.module_slots * 2
+
+if settings.startup["show-module-slot-row-length"].value > 0 then
+  beaconed_lab_3.module_specification.module_info_max_icons_per_row = settings.startup["show-module-slot-row-length"].value
+end
+if settings.startup["show-module-slot-rows"].value > 0 then
+  beaconed_lab_3.module_specification.module_info_max_icon_rows = settings.startup["show-module-slot-rows"].value
+end
+
+if settings.startup["modded-entity-graphics"].value == true then
+  beaconed_lab_3.on_animation = 
+  {
+    layers =
+    {
+      {
+        filename = "__Absorbed-Beacons__/graphics/entity/beaconed-lab/lab-space.png",
+        width = 98,
+        height = 87,
+        frame_count = 33,
+        line_length = 11,
+        animation_speed = 1 / 3,
+        shift = util.by_pixel(0, 1.5),
+        hr_version =
+        {
+          filename = "__Absorbed-Beacons__/graphics/entity/beaconed-lab/hr-lab-space.png",
+          width = 194,
+          height = 174,
+          frame_count = 33,
+          line_length = 11,
+          animation_speed = 1 / 3,
+          shift = util.by_pixel(0, 1.5),
+          scale = 0.5
+        }
+      },
+      {
+        filename = "__base__/graphics/entity/lab/lab-integration.png",
+        width = 122,
+        height = 81,
+        frame_count = 1,
+        line_length = 1,
+        repeat_count = 33,
+        animation_speed = 1 / 3,
+        shift = util.by_pixel(0, 15.5),
+        hr_version =
+        {
+          filename = "__base__/graphics/entity/lab/hr-lab-integration.png",
+          width = 242,
+          height = 162,
+          frame_count = 1,
+          line_length = 1,
+          repeat_count = 33,
+          animation_speed = 1 / 3,
+          shift = util.by_pixel(0, 15.5),
+          scale = 0.5
+        }
+      },
+      {
+        filename = "__base__/graphics/entity/lab/lab-shadow.png",
+        width = 122,
+        height = 68,
+        frame_count = 1,
+        line_length = 1,
+        repeat_count = 33,
+        animation_speed = 1 / 3,
+        shift = util.by_pixel(13, 11),
+        draw_as_shadow = true,
+        hr_version =
+        {
+          filename = "__base__/graphics/entity/lab/hr-lab-shadow.png",
+          width = 242,
+          height = 136,
+          frame_count = 1,
+          line_length = 1,
+          repeat_count = 33,
+          animation_speed = 1 / 3,
+          shift = util.by_pixel(13, 11),
+          scale = 0.5,
+          draw_as_shadow = true
+        }
+      }
+    }
+  }
+end
+
+data:extend({
+  beaconed_lab_3
+})
+data:extend({
+  {
+    type = "item",
+    name = "beaconed-lab-3",
+    icon = "__Absorbed-Beacons__/graphics/icons/beaconed-lab-3.png",
+    icon_size = 32,
+    subgroup = "absorbed-beacons-3",
+    order = "g[lab]",
+    place_result = "beaconed-lab-3",
+    stack_size = 10
+  },
+})
 data:extend({
   {
     type = "recipe",
@@ -19,197 +128,4 @@ data:extend({
     allow_as_intermediate = false,
     main_product = "beaconed-lab-3"
   }
-})
-data:extend({
-  {
-    type = "item",
-    name = "beaconed-lab-3",
-    icon = "__Absorbed-Beacons__/graphics/icons/beaconed-lab-3.png",
-    icon_size = 32,
-    subgroup = "absorbed-beacons-3",
-    order = "g[lab]",
-    place_result = "beaconed-lab-3",
-    stack_size = 10
-  },
-})
-data:extend({
-  {
-    type = "lab",
-    name = "beaconed-lab-3",
-    icon = "__Absorbed-Beacons__/graphics/icons/beaconed-lab-3.png",
-    icon_size = 32,
-    flags = {"placeable-player", "player-creation"},
-    minable = {mining_time = 0.2, result = "beaconed-lab-3"},
-    max_health = 150,
-    corpse = "big-remnants",
-    dying_explosion = "medium-explosion",
-    collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
-    selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
-    light = {intensity = 0.75, size = 8, color = {r = 1.0, g = 1.0, b = 1.0}},
-    fast_replaceable_group = "lab",
-    on_animation =
-    {
-      layers =
-      {
-        {
-          filename = "__Absorbed-Beacons__/graphics/entity/beaconed-lab/lab-space.png",
-          width = 98,
-          height = 87,
-          frame_count = 33,
-          line_length = 11,
-          animation_speed = 1 / 3,
-          shift = util.by_pixel(0, 1.5),
-          hr_version =
-          {
-            filename = "__Absorbed-Beacons__/graphics/entity/beaconed-lab/hr-lab-space.png",
-            width = 194,
-            height = 174,
-            frame_count = 33,
-            line_length = 11,
-            animation_speed = 1 / 3,
-            shift = util.by_pixel(0, 1.5),
-            scale = 0.5
-          }
-        },
-        {
-          filename = "__base__/graphics/entity/lab/lab-integration.png",
-          width = 122,
-          height = 81,
-          frame_count = 1,
-          line_length = 1,
-          repeat_count = 33,
-          animation_speed = 1 / 3,
-          shift = util.by_pixel(0, 15.5),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/lab/hr-lab-integration.png",
-            width = 242,
-            height = 162,
-            frame_count = 1,
-            line_length = 1,
-            repeat_count = 33,
-            animation_speed = 1 / 3,
-            shift = util.by_pixel(0, 15.5),
-            scale = 0.5
-          }
-        },
-        {
-          filename = "__base__/graphics/entity/lab/lab-shadow.png",
-          width = 122,
-          height = 68,
-          frame_count = 1,
-          line_length = 1,
-          repeat_count = 33,
-          animation_speed = 1 / 3,
-          shift = util.by_pixel(13, 11),
-          draw_as_shadow = true,
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/lab/hr-lab-shadow.png",
-            width = 242,
-            height = 136,
-            frame_count = 1,
-            line_length = 1,
-            repeat_count = 33,
-            animation_speed = 1 / 3,
-            shift = util.by_pixel(13, 11),
-            scale = 0.5,
-            draw_as_shadow = true
-          }
-        }
-      }
-    },
-    off_animation =
-    {
-      layers =
-      {
-        {
-          filename = "__base__/graphics/entity/lab/lab.png",
-          width = 98,
-          height = 87,
-          frame_count = 1,
-          shift = util.by_pixel(0, 1.5),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/lab/hr-lab.png",
-            width = 194,
-            height = 174,
-            frame_count = 1,
-            shift = util.by_pixel(0, 1.5),
-            scale = 0.5
-          }
-        },
-        {
-          filename = "__base__/graphics/entity/lab/lab-integration.png",
-          width = 122,
-          height = 81,
-          frame_count = 1,
-          shift = util.by_pixel(0, 15.5),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/lab/hr-lab-integration.png",
-            width = 242,
-            height = 162,
-            frame_count = 1,
-            shift = util.by_pixel(0, 15.5),
-            scale = 0.5
-          }
-        },
-        {
-          filename = "__base__/graphics/entity/lab/lab-shadow.png",
-          width = 122,
-          height = 68,
-          frame_count = 1,
-          shift = util.by_pixel(13, 11),
-          draw_as_shadow = true,
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/lab/hr-lab-shadow.png",
-            width = 242,
-            height = 136,
-            frame_count = 1,
-            shift = util.by_pixel(13, 11),
-            draw_as_shadow = true,
-            scale = 0.5
-          }
-        }
-      }
-    },
-    working_sound =
-    {
-      sound =
-      {
-        filename = "__base__/sound/lab.ogg",
-        volume = 0.7
-      },
-      apparent_volume = 1
-    },
-    vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
-    energy_source =
-    {
-      type = "electric",
-      usage_priority = "secondary-input",
-      drain = "3360kW",
-    },
-    energy_usage = "660kW",
-    researching_speed = 6.7,
-    inputs =
-    {
-      "automation-science-pack",
-      "logistic-science-pack",
-      "chemical-science-pack",
-      "military-science-pack",
-      "production-science-pack",
-      "utility-science-pack",
-      "space-science-pack"
-    },
-    allowed_effects = {"productivity", "pollution"},
-    module_specification =
-    {
-      module_slots = 4,
-      module_info_max_icons_per_row = 1,
-      module_info_max_icon_rows = 1,
-      module_info_icon_shift = {0, 0.9}
-    }
-  },
 })
