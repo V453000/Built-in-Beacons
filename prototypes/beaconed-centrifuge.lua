8c849fb53979bd484bf7548f2b01f7ce29e1f77e
Vaclav Benc <vaclav.benc@factorio.com>
2019-07-14 16:18:16 +0200

    Rewrote beaconed centrifuges.

--- prototypes/beaconed-centrifuge.lua
+++ prototypes/beaconed-centrifuge.lua
@@ -1,3 +1,38 @@
+beaconed_centrifuge = util.table.deepcopy(data.raw["assembling-machine"]["centrifuge"])
+beaconed_centrifuge.name = "beaconed-centrifuge"
+beaconed_centrifuge.icon = "__Absorbed-Beacons__/graphics/icons/beaconed-centrifuge.png"
+beaconed_centrifuge.minable.result = "beaconed-centrifuge"
+beaconed_centrifuge.next_upgrade = "beaconed-centrifuge-2"
+beaconed_centrifuge.crafting_speed = 3.1
+beaconed_centrifuge.energy_source.emissions_per_second_per_watt = 4 / 350000
+beaconed_centrifuge.energy_source.drain = "3372kW"
+beaconed_centrifuge.energy_usage = "2730kW"
+beaconed_centrifuge.allowed_effects = {"productivity", "pollution"}
+beaconed_centrifuge.fast_replaceable_group = "centrifuge"
+
+beaconed_centrifuge.module_specification.module_slots = data.raw["assembling-machine"]["centrifuge"].module_specification.module_slots * 2
+if settings.startup["show-module-slot-row-length"].value > 0 then
+  beaconed_centrifuge.module_specification.module_info_max_icons_per_row = settings.startup["show-module-slot-row-length"].value
+end
+if settings.startup["show-module-slot-rows"].value > 0 then
+  beaconed_centrifuge.module_specification.module_info_max_icon_rows = settings.startup["show-module-slot-rows"].value
+end
+
+data:extend({
+  beaconed_centrifuge
+})
+data:extend({
+  {
+    type = "item",
+    name = "beaconed-centrifuge",
+    icon = "__Absorbed-Beacons__/graphics/icons/beaconed-centrifuge.png",
+    icon_size = 32,
+    subgroup = "absorbed-beacons",
+    order = "g[centrifuge]",
+    place_result = "beaconed-centrifuge",
+    stack_size = 50
+  },
+})
 data:extend({
   {
     type = "recipe",
@@ -18,317 +53,4 @@ data:extend({
     },
     allow_as_intermediate = false
   }
-})
-data:extend({
-  {
-    type = "item",
-    name = "beaconed-centrifuge",
-    icon = "__Absorbed-Beacons__/graphics/icons/beaconed-centrifuge.png",
-    icon_size = 32,
-    subgroup = "absorbed-beacons",
-    order = "g[centrifuge]",
-    place_result = "beaconed-centrifuge",
-    stack_size = 50
-  },
-})
-
-data:extend({
-  {
-    type = "assembling-machine",
-    name = "beaconed-centrifuge",
-    icon = "__Absorbed-Beacons__/graphics/icons/beaconed-centrifuge.png",
-    icon_size = 32,
-    flags = {"placeable-neutral", "placeable-player", "player-creation"},
-    minable = {mining_time = 0.1, result = "beaconed-centrifuge"},
-    max_health = 350,
-    corpse = "big-remnants",
-    dying_explosion = "medium-explosion",
-    resistances =
-    {
-      {
-        type = "fire",
-        percent = 70
-      }
-    },
-    collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
-    selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
-    drawing_box = {{-1.5, -2.2}, {1.5, 1.5}},
-    fast_replaceable_group = "centrifuge",
-    next_upgrade = "beaconed-centrifuge-2",
-    always_draw_idle_animation = true,
-    idle_animation =
-    {
-      layers =
-      {
-        -- Centrifuge A
-        {
-          filename = "__base__/graphics/entity/centrifuge/centrifuge-C.png",
-          priority = "high",
-          line_length = 8,
-          width = 119,
-          height = 107,
-          frame_count = 64,
-          animation_speed = beaconed_centrifuge_1_animation_speed,
-          shift = util.by_pixel(-0.5, -26.5),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/centrifuge/hr-centrifuge-C.png",
-            priority = "high",
-            scale = 0.5,
-            line_length = 8,
-            width = 237,
-            height = 214,
-            frame_count = 64,
-            animation_speed = beaconed_centrifuge_1_animation_speed,
-            shift = util.by_pixel(-0.25, -26.5)
-          }
-        },
-        {
-          filename = "__base__/graphics/entity/centrifuge/centrifuge-C-shadow.png",
-          draw_as_shadow = true,
-          priority = "high",
-          line_length = 8,
-          width = 132,
-          height = 74,
-          frame_count = 64,
-          animation_speed = beaconed_centrifuge_1_animation_speed,
-          shift = util.by_pixel(20, -10),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/centrifuge/hr-centrifuge-C-shadow.png",
-            draw_as_shadow = true,
-            priority = "high",
-            scale = 0.5,
-            line_length = 8,
-            width = 279,
-            height = 152,
-            frame_count = 64,
-            animation_speed = beaconed_centrifuge_1_animation_speed,
-            shift = util.by_pixel(16.75, -10)
-          }
-        },
-        -- Centrifuge B
-        {
-          filename = "__base__/graphics/entity/centrifuge/centrifuge-B.png",
-          priority = "high",
-          line_length = 8,
-          width = 78,
-          height = 117,
-          frame_count = 64,
-          animation_speed = beaconed_centrifuge_1_animation_speed,
-          shift = util.by_pixel(23, 6.5),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/centrifuge/hr-centrifuge-B.png",
-            priority = "high",
-            scale = 0.5,
-            line_length = 8,
-            width = 156,
-            height = 234,
-            frame_count = 64,
-            animation_speed = beaconed_centrifuge_1_animation_speed,
-            shift = util.by_pixel(23, 6.5)
-          }
-        },
-        {
-          filename = "__base__/graphics/entity/centrifuge/centrifuge-B-shadow.png",
-          draw_as_shadow = true,
-          priority = "high",
-          line_length = 8,
-          width = 124,
-          height = 74,
-          frame_count = 64,
-          animation_speed = beaconed_centrifuge_1_animation_speed,
-          shift = util.by_pixel(63, 16),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/centrifuge/hr-centrifuge-B-shadow.png",
-            draw_as_shadow = true,
-            priority = "high",
-            scale = 0.5,
-            line_length = 8,
-            width = 251,
-            height = 149,
-            frame_count = 64,
-            animation_speed = beaconed_centrifuge_1_animation_speed,
-            shift = util.by_pixel(63.25, 15.25)
-          }
-        },
-        -- Centrifuge A
-        {
-          filename = "__base__/graphics/entity/centrifuge/centrifuge-A.png",
-          priority = "high",
-          line_length = 8,
-          width = 70,
-          height = 123,
-          frame_count = 64,
-          animation_speed = beaconed_centrifuge_1_animation_speed,
-          shift = util.by_pixel(-26, 3.5),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/centrifuge/hr-centrifuge-A.png",
-            priority = "high",
-            scale = 0.5,
-            line_length = 8,
-            width = 139,
-            height = 246,
-            frame_count = 64,
-            animation_speed = beaconed_centrifuge_1_animation_speed,
-            shift = util.by_pixel(-26.25, 3.5)
-          }
-        },
-        {
-          filename = "__base__/graphics/entity/centrifuge/centrifuge-A-shadow.png",
-          priority = "high",
-          draw_as_shadow = true,
-          line_length = 8,
-          width = 108,
-          height = 54,
-          frame_count = 64,
-          animation_speed = beaconed_centrifuge_1_animation_speed,
-          shift = util.by_pixel(6, 27),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/centrifuge/hr-centrifuge-A-shadow.png",
-            priority = "high",
-            draw_as_shadow = true,
-            scale = 0.5,
-            line_length = 8,
-            width = 230,
-            height = 124,
-            frame_count = 64,
-            animation_speed = beaconed_centrifuge_1_animation_speed,
-            shift = util.by_pixel(8.5, 23.5)
-          }
-        }
-      }
-    },
-
-    animation =
-    {
-      layers =
-      {
-        -- Centrifuge C
-        {
-          filename = "__base__/graphics/entity/centrifuge/centrifuge-C-light.png",
-          priority = "high",
-          blend_mode = "additive", -- centrifuge
-          line_length = 8,
-          width = 96,
-          height = 104,
-          frame_count = 64,
-          animation_speed = beaconed_centrifuge_1_animation_speed,
-          shift = util.by_pixel(0, -27),
-
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/centrifuge/hr-centrifuge-C-light.png",
-            priority = "high",
-            scale = 0.5,
-            blend_mode = "additive", -- centrifuge
-            line_length = 8,
-            width = 190,
-            height = 207,
-            frame_count = 64,
-            animation_speed = beaconed_centrifuge_1_animation_speed,
-            shift = util.by_pixel(0, -27.25)
-          }
-        },
-        -- Centrifuge B
-        {
-          filename = "__base__/graphics/entity/centrifuge/centrifuge-B-light.png",
-          priority = "high",
-          blend_mode = "additive", -- centrifuge
-          line_length = 8,
-          width = 65,
-          height = 103,
-          frame_count = 64,
-          animation_speed = beaconed_centrifuge_1_animation_speed,
-          shift = util.by_pixel(16.5, 0.5),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/centrifuge/hr-centrifuge-B-light.png",
-            priority = "high",
-            scale = 0.5,
-            blend_mode = "additive", -- centrifuge
-            line_length = 8,
-            width = 131,
-            height = 206,
-            frame_count = 64,
-            animation_speed = beaconed_centrifuge_1_animation_speed,
-            shift = util.by_pixel(16.75, 0.5)
-          }
-        },
-        -- Centrifuge A
-        {
-          filename = "__base__/graphics/entity/centrifuge/centrifuge-A-light.png",
-          priority = "high",
-          blend_mode = "additive", -- centrifuge
-          line_length = 8,
-          width = 55,
-          height = 98,
-          frame_count = 64,
-          animation_speed = beaconed_centrifuge_1_animation_speed,
-          shift = util.by_pixel(-23.5, -2),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/centrifuge/hr-centrifuge-A-light.png",
-            priority = "high",
-            scale = 0.5,
-            blend_mode = "additive", -- centrifuge
-            line_length = 8,
-            width = 108,
-            height = 197,
-            frame_count = 64,
-            animation_speed = beaconed_centrifuge_1_animation_speed,
-            shift = util.by_pixel(-23.5, -1.75)
-          }
-        }
-      }
-    },
-
-    working_visualisations =
-    {
-      {
-        effect = "uranium-glow", -- changes alpha based on energy source light intensity
-        light = {intensity = 0.6, size = 9.9, shift = {0.0, 0.0}, color = {r = 0.0, g = 1.0, b = 0.0}}
-      }
-    },
-    open_sound = { filename = "__base__/sound/machine-open.ogg", volume = 0.85 },
-    close_sound = { filename = "__base__/sound/machine-close.ogg", volume = 0.75 },
-    vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
-    working_sound =
-    {
-      sound =
-      {
-        {
-          filename = "__base__/sound/assembling-machine-t2-1.ogg",
-          volume = 0.8
-        },
-        {
-          filename = "__base__/sound/assembling-machine-t2-2.ogg",
-          volume = 0.8
-        }
-      },
-      idle_sound = { filename = "__base__/sound/idle1.ogg", volume = 0.6 },
-      apparent_volume = 1.5
-    },
-    crafting_speed = 3.1,
-    crafting_categories = {"centrifuging"},
-    energy_source =
-    {
-      type = "electric",
-      usage_priority = "secondary-input",
-      emissions_per_second_per_watt = 4 / 350000,
-      drain = "3372kW"
-    },
-    energy_usage = "2730kW",
-    module_specification =
-    {
-      module_slots = 4,
-      module_info_max_icons_per_row = 1,
-      module_info_max_icon_rows = 1,
-    },
-    allowed_effects = {"productivity", "pollution"}
-  },
 })
