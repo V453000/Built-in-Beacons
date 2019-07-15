bc8beecfd8ca0baf3ca6b3157052b002c2b03e61
Vaclav Benc <vaclav.benc@factorio.com>
2019-07-15 13:37:32 +0200

    Beaconed lab graphics rewrite.

--- prototypes/beaconed-lab-2.lua
+++ prototypes/beaconed-lab-2.lua
@@ -18,79 +18,37 @@ if settings.startup["show-module-slot-rows"].value > 0 then
   beaconed_lab_2.module_specification.module_info_max_icon_rows = settings.startup["show-module-slot-rows"].value
 end
 
-if settings.startup["modded-entity-graphics"].value == true then
-  beaconed_lab_2.on_animation = 
-  {
-    layers =
+if settings.startup["modded-entity-graphics"].value == "ON" then
+  beaconed_lab_2.on_animation.layers[1] = 
     {
+      filename = "__Absorbed-Beacons__/graphics/entity/beaconed-lab/lab-yellow.png",
+      width = 98,
+      height = 87,
+      frame_count = 33,
+      line_length = 11,
+      animation_speed = 1 / 3,
+      shift = util.by_pixel(0, 1.5),
+      hr_version =
       {
-        filename = "__Absorbed-Beacons__/graphics/entity/beaconed-lab/lab-yellow.png",
-        width = 98,
-        height = 87,
+        filename = "__Absorbed-Beacons__/graphics/entity/beaconed-lab/hr-lab-yellow.png",
+        width = 194,
+        height = 174,
         frame_count = 33,
         line_length = 11,
         animation_speed = 1 / 3,
         shift = util.by_pixel(0, 1.5),
-        hr_version =
-        {
-          filename = "__Absorbed-Beacons__/graphics/entity/beaconed-lab/hr-lab-yellow.png",
-          width = 194,
-          height = 174,
-          frame_count = 33,
-          line_length = 11,
-          animation_speed = 1 / 3,
-          shift = util.by_pixel(0, 1.5),
-          scale = 0.5
-        }
-      },
-      {
-        filename = "__base__/graphics/entity/lab/lab-integration.png",
-        width = 122,
-        height = 81,
-        frame_count = 1,
-        line_length = 1,
-        repeat_count = 33,
-        animation_speed = 1 / 3,
-        shift = util.by_pixel(0, 15.5),
-        hr_version =
-        {
-          filename = "__base__/graphics/entity/lab/hr-lab-integration.png",
-          width = 242,
-          height = 162,
-          frame_count = 1,
-          line_length = 1,
-          repeat_count = 33,
-          animation_speed = 1 / 3,
-          shift = util.by_pixel(0, 15.5),
-          scale = 0.5
-        }
-      },
-      {
-        filename = "__base__/graphics/entity/lab/lab-shadow.png",
-        width = 122,
-        height = 68,
-        frame_count = 1,
-        line_length = 1,
-        repeat_count = 33,
-        animation_speed = 1 / 3,
-        shift = util.by_pixel(13, 11),
-        draw_as_shadow = true,
-        hr_version =
-        {
-          filename = "__base__/graphics/entity/lab/hr-lab-shadow.png",
-          width = 242,
-          height = 136,
-          frame_count = 1,
-          line_length = 1,
-          repeat_count = 33,
-          animation_speed = 1 / 3,
-          shift = util.by_pixel(13, 11),
-          scale = 0.5,
-          draw_as_shadow = true
-        }
+        scale = 0.5
       }
     }
-  }
+end
+
+if settings.startup["modded-entity-graphics"].value ~= "OFF" then
+  for i,layer in pairs(beaconed_lab_2.on_animation.layers) do
+    layer.animation_speed = beaconed_lab_2_animation_speed
+    if (layer.hr_version) then
+      layer.hr_version.animation_speed = beaconed_lab_2_animation_speed
+    end
+  end
 end
 
 data:extend({
