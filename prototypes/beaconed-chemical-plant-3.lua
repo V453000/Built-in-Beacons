8e1dded0ca5d6d0316dad1af48c8b5ba565ed498
Vaclav Benc <vaclav.benc@factorio.com>
2019-07-15 12:29:04 +0200

    Beaconed chemical plant animation speeds.

--- prototypes/beaconed-chemical-plant-3.lua
+++ prototypes/beaconed-chemical-plant-3.lua
@@ -17,6 +17,25 @@ if settings.startup["show-module-slot-rows"].value > 0 then
   beaconed_chemical_plant_3.module_specification.module_info_max_icon_rows = settings.startup["show-module-slot-rows"].value
 end
 
+if settings.startup["modded-entity-graphics"].value ~= "OFF" then
+  local set_animation_speed = function(anim)
+    if anim then
+      anim.animation_speed = beaconed_chemical_plant_3_animation_speed
+      if (anim.hr_version) then
+        anim.hr_version.animation_speed = beaconed_chemical_plant_3_animation_speed
+      end
+    end 
+  end
+
+  for i,layer in pairs(beaconed_chemical_plant_3.working_visualisations) do
+    set_animation_speed(layer.animation)
+    --set_animation_speed(layer.north_animation)
+    --set_animation_speed(layer.south_animation)
+    --set_animation_speed(layer.east_animation)
+    --set_animation_speed(layer.west_animation)
+  end
+end
+
 data:extend({
   beaconed_chemical_plant_3
 })
--- prototypes/beaconed-chemical-plant.lua
+++ prototypes/beaconed-chemical-plant.lua
@@ -18,6 +18,26 @@ if settings.startup["show-module-slot-rows"].value > 0 then
   beaconed_chemical_plant.module_specification.module_info_max_icon_rows = settings.startup["show-module-slot-rows"].value
 end
 
+
+if settings.startup["modded-entity-graphics"].value ~= "OFF" then
+  local set_animation_speed = function(anim)
+    if anim then
+      anim.animation_speed = beaconed_chemical_plant_1_animation_speed
+      if (anim.hr_version) then
+        anim.hr_version.animation_speed = beaconed_chemical_plant_1_animation_speed
+      end
+    end 
+  end
+
+  for i,layer in pairs(beaconed_chemical_plant.working_visualisations) do
+    set_animation_speed(layer.animation)
+    --set_animation_speed(layer.north_animation)
+    --set_animation_speed(layer.south_animation)
+    --set_animation_speed(layer.east_animation)
+    --set_animation_speed(layer.west_animation)
+  end
+end
+
 data:extend({
   beaconed_chemical_plant
 })
