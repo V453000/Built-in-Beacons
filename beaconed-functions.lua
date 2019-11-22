d6e74b9e2ce053d35531b99b8ae64a1b39d9d56e
Vaclav Benc <vaclav.benc@factorio.com>
2019-11-22 11:02:07 +0100

    Fixed reading module effects.

--- beaconed-functions.lua
+++ beaconed-functions.lua
@@ -1,18 +1,18 @@
 global_beacon_transmission_effect = data.raw["beacon"]["beacon"].distribution_effectivity --or 0
 global_beacon_module_slots = data.raw["beacon"]["beacon"].module_specification.module_slots --or 0
 global_beacon_drain = data.raw["beacon"]["beacon"].energy_usage -- or 0
-global_speed_module_1_speed_bonus = data.raw["module"]["speed-module"].effect.speed --or 0
-global_speed_module_2_speed_bonus = data.raw["module"]["speed-module-2"].effect.speed --or 0
-global_speed_module_3_speed_bonus = data.raw["module"]["speed-module-3"].effect.speed --or 0
-global_productivity_module_1_speed_bonus = data.raw["module"]["productivity-module"].effect.speed --or 0
-global_productivity_module_2_speed_bonus = data.raw["module"]["productivity-module-2"].effect.speed --or 0
-global_productivity_module_3_speed_bonus = data.raw["module"]["productivity-module-3"].effect.speed --or 0
-global_speed_module_1_consumption_bonus = data.raw["module"]["speed-module"].effect.speed --or 0
-global_speed_module_2_consumption_bonus = data.raw["module"]["speed-module-2"].effect.speed --or 0
-global_speed_module_3_consumption_bonus = data.raw["module"]["speed-module-3"].effect.speed --or 0
-global_productivity_module_1_consumption_bonus = data.raw["module"]["productivity-module"].effect.consumption --or 0
-global_productivity_module_2_consumption_bonus = data.raw["module"]["productivity-module-2"].effect.consumption --or 0
-global_productivity_module_3_consumption_bonus = data.raw["module"]["productivity-module-3"].effect.consumption --or 0
+global_speed_module_1_speed_bonus = data.raw["module"]["speed-module"].effect.speed.bonus --or 0
+global_speed_module_2_speed_bonus = data.raw["module"]["speed-module-2"].effect.speed.bonus --or 0
+global_speed_module_3_speed_bonus = data.raw["module"]["speed-module-3"].effect.speed.bonus --or 0
+global_productivity_module_1_speed_bonus = data.raw["module"]["productivity-module"].effect.speed.bonus --or 0
+global_productivity_module_2_speed_bonus = data.raw["module"]["productivity-module-2"].effect.speed.bonus --or 0
+global_productivity_module_3_speed_bonus = data.raw["module"]["productivity-module-3"].effect.speed.bonus --or 0
+global_speed_module_1_consumption_bonus = data.raw["module"]["speed-module"].effect.speed.bonus --or 0
+global_speed_module_2_consumption_bonus = data.raw["module"]["speed-module-2"].effect.speed.bonus --or 0
+global_speed_module_3_consumption_bonus = data.raw["module"]["speed-module-3"].effect.speed.bonus --or 0
+global_productivity_module_1_consumption_bonus = data.raw["module"]["productivity-module"].effect.consumption.bonus --or 0
+global_productivity_module_2_consumption_bonus = data.raw["module"]["productivity-module-2"].effect.consumption.bonus --or 0
+global_productivity_module_3_consumption_bonus = data.raw["module"]["productivity-module-3"].effect.consumption.bonus --or 0
 
 global_assembling_machine_beacon_count    = 12
 global_electric_furnace_beacon_count      = 12
@@ -35,8 +35,8 @@ function beaconed_crafting_speed(params)
 
   local beacon_speed_effect         = beacon_count * beacon_module_speed_bonus * beacon_effect * beacon_module_slots
   local machine_module_speed_effect = machine_module_slots * machine_module_speed_bonus
-  local speed_multiplier            = 1 + (machine_module_speed_effect + beacon_speed_effect)/100
+  local speed_multiplier            = 1 + machine_module_speed_effect + beacon_speed_effect
   local beaconed_crafting_speed     = machine_crafting_speed * speed_multiplier
   --machine_crafting_speed * (1 + (( (machine_module_slots * machine_module_speed_bonus) + (beacon_count*beacon_module_speed_bonus*beacon_effect*beacon_module_slots) )/100))))
-  return beaconed_cracting_speed
+  return beaconed_crafting_speed
 end
